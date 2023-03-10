3d53c8cf0261bc7b08b270c8a671a30859db4f3b
Alirza Fekri <alirezafekri2003@gmail.com>
2023-03-10 20:15:32 +0330

    add soft delete and ERD

--- contacUsQuery.sql
+++ contacUsQuery.sql
@@ -1,4 +1,6 @@
 -- query insert
 INSERT into contact(fullname,attach,title,description,email,state) VALUES("User1","path save","first title","test@gmail.com","test massage","Active")
--- querry Delete
+-- querry hard Delete
 Delete from contact where id = 1
+-- querry soft Delete
+update users set state = 'Deleted' WHERE id = 0
--- usersQuery.sql
+++ usersQuery.sql
@@ -1,4 +1,6 @@
 -- query insert
 insert into users(full_name,password,settlement_account,post,phone_number,address,state) VALUES("Alireza Fekri","sdf3",0340,"Admin",939123456,"isfahan","Active")
--- querry Delete
+-- querry hard Delete
 Delete from users where id = 1
+-- querry soft Delete
+update users set state = 'DeActive' WHERE id = 0
